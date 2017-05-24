c81c78224bfcb03f477a58791fe2c1a107ca85a0
koike <r_ko@live.jp>
2017-05-24 23:52:24 +0900

    :up: Updated files

--- Request.php
+++ Request.php
@@ -80,4 +80,77 @@ class Request
             ];
         }
     }
+
+    public static function post(string $url, string $ref=null) : array
+    {
+        if(!is_string($url) || strlen($url) == 0)
+        {
+            return
+            [
+                'status'    =>  400,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+
+        // IE 8 + Windows XP
+        $ua = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/7.0; rv:11.0; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648)';
+
+        // IE 10 + Windows 8
+        // $ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+        $ref = $url;
+
+        $client = new Client(['verify' => false]);
+        try
+        {
+            $response = $client->request
+            (
+                'POST',
+                $url,
+                [
+                    'headers'   =>
+                    [
+                        'User-Agent'    =>  $ua,
+                        'Referer'       =>  $ref
+                    ],
+                    'timeout'   =>  5
+                ]
+            );
+
+            return
+            [
+                'status'    =>  $response->getStatusCode(),
+                'type'      =>  $response->getHeader('Content-Type'),
+                'body'      =>  $response->getBody()
+            ];
+        }
+        catch(\Error $e)
+        {
+            return
+            [
+                'status'    =>  500,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+        catch(\Exception $e)
+        {
+            return
+            [
+                'status'    =>  500,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+        catch(\Throwable $t)
+        {
+            return
+            [
+                'status'    =>  500,
+                'type'      =>  null,
+                'body'      =>  null
+            ];
+        }
+    }
 }
--- main.php
+++ main.php
@@ -75,23 +75,34 @@ if($response['status'] < 200 || $response['status'] >= 400)
 $html = $response['body'] . '';
 file_put_contents($filename . '_1.html', $html);
 
+$url = explode("'", explode("var NormalURL = '", $html)[1])[0];
+
+$response = Request::post($url, $old_url);
+if($reponse['status'] < 200 || $response['status'] >= 400)
+{
+    echo '[!] HTTP Status: ' . $response['status'] . PHP_EOL;
+    exit(-1);
+}
+$html = $response['body'] . '';
+file_put_contents($filename . '_2.html', $html);
+
 $html = explode("\n", $html);
 
 // IE 6
-$js[0][0] = substr(trim($html[3]), 34);
-$js[0][1] = substr(trim($html[4]), 12);
-$js[1][0] = substr(trim($html[10]), 27);
-$js[1][1] = substr(trim($html[11]), 12);
-$js[2][0] = substr(trim($html[17]), 27);
-$js[2][1] = substr(trim($html[18]), 12);
-
-// IE 8
-// $js[0][0] = substr(trim($html[5]), 27);
-// $js[0][1] = substr(trim($html[6]), 12);
+// $js[0][0] = substr(trim($html[3]), 34);
+// $js[0][1] = substr(trim($html[4]), 12);
 // $js[1][0] = substr(trim($html[10]), 27);
 // $js[1][1] = substr(trim($html[11]), 12);
-// $js[2][0] = substr(trim($html[15]), 27);
-// $js[2][1] = substr(trim($html[16]), 12);
+// $js[2][0] = substr(trim($html[17]), 27);
+// $js[2][1] = substr(trim($html[18]), 12);
+
+// IE 8
+$js[0][0] = substr(trim($html[5]), 27);
+$js[0][1] = substr(trim($html[6]), 12);
+$js[1][0] = substr(trim($html[10]), 27);
+$js[1][1] = substr(trim($html[11]), 12);
+$js[2][0] = substr(trim($html[15]), 27);
+$js[2][1] = substr(trim($html[16]), 12);
 
 $split[0][0] = substr($js[0][0], -4, 1);
 $split[0][1] = substr($js[0][1], -4, 1);
